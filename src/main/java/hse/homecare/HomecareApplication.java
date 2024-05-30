5dc9d70f25998ec4d2da825139fdd0aedbd6043a
Sardor Sharipov <sardorsharipov2000@mail.ru>
2024-05-30 18:17:03 +0300

    update

--- src/main/java/hse/homecare/HomecareApplication.java
+++ src/main/java/hse/homecare/HomecareApplication.java
@@ -2,22 +2,12 @@ package hse.homecare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@Configuration
-public class HomecareApplication implements WebMvcConfigurer {
+public class HomecareApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HomecareApplication.class, args);
-    }
-
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/pngFiles/**")
-                .addResourceLocations("file:ext-resources/")
-                .setCachePeriod(0);
     }
 }
 
